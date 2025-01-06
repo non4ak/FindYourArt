47b96c75214cd548a56b6532ca02d2a5263c5084
non4ak <andriy010706@gmail.com>
2025-01-07 01:46:42 +0200

    first

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -5,7 +5,10 @@ module.exports.getPicture = async (req, res) => {
         const isLoggedIn = !!req.session.user; 
         const query = `SELECT * FROM paintings WHERE painting_id = ${req.params.id}`;
         const data = await runDBcommand(query);
-        res.render("picture.ejs", {paintings: data[0], isLoggedIn})
+
+        const artistQuery = `SELECT a.*, COUNT(p.painting_id) AS paintings_count, SUM(p.price) AS total_price, COUNT(CASE WHEN p.availability = 'sold' THEN 1 END) AS sold_paintings_count FROM artists a LEFT JOIN paintings p ON a.artist_id = p.artist_id WHERE p.painting_id = ${req.params.id} GROUP BY a.artist_id`;
+        const artistData = await runDBcommand(artistQuery)
+        res.render("picture.ejs", {paintings: data[0], isLoggedIn, artist: artistData[0]});
     }
     catch(err) {
         console.log(err)
